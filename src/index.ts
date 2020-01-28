cd445df18a2ce9b3c84f81a49de74d712100c6e4
udit.maheshwari@gmail.com <Yudi@1298>
2020-01-28 12:22:23 +0530

    implemented IItem in ItemPlayer file

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,84 @@ import { IItem, IInitOptions, IValidationStatus } from "libs-interface-item-play
 
 class ItemPlayer implements IItem {
 
-    //TBD
+    destroy() {
+    //TBD    
+    }
+
+    focalise() {
+        //TBD
+    }
+
+    getOptimumDimensions() {
+        //TBD
+        return null;
+    }
+
+    getState() {
+        //TBD
+    }
+
+    hasHints() {
+        //TBD
+        return null;
+    }
+
+    hide() {
+        //TBD
+    }
+
+    hideHints() {
+        //TBD
+    }
+
+    initItem(uid: string, container: HTMLElement, initOptions: IInitOptions) {
+        //TBD
+    }
+
+    lock() {
+        //TBD
+    }
+
+    redo(options: any) {
+        //TBD
+    }
+
+    remainingHints() {
+        //TBD
+        return null;
+    }
+
+    resetDimensions() {
+        //TBD
+    }
+
+    setState(state: any) {
+        //TBD
+    }
+
+    show() {
+        //TBD
+    }
+
+    showHints() {
+        //TBD
+        return null;
+    }
+
+    subscribe(eventName: string, eventHandler: Function) {
+        //TBD
+    }
+
+    unlock() {
+        //TBD
+    }
+
+    unsubscribe(eventName: string, eventHandler: Function) {
+        //TBD
+    }
+
+    validateResponse() {
+        //TBD
+        return null;
+    }
 }
